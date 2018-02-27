fba0e16a58a4b8ed75b8a8f242c7b9b71db34149
Nahuel Ortelli <nortelli@iphoideas.com>
2018-02-27 11:39:14 -0300

    code reader

---
+++ app/src/main/java/com/napps/nutriapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.napps.nutriapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    public static TextView tvresult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvresult = (TextView) findViewById(R.id.tvresult);
+
+        Button btn = (Button) findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScanActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
